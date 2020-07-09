709fb6e56c964fbdafe23229216fdc90c80f65b8
tsth <1349810446@qq.com>
2020-07-09 22:09:32 +0800

    day01

---
+++ mymall01/src/main/java/com/xcs/mymall01/Mymall01Application.java
@@ -0,0 +1,13 @@
+package com.xcs.mymall01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mymall01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mymall01Application.class, args);
+    }
+
+}
---
+++ mymall01/src/test/java/com/xcs/mymall01/Mymall01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcs.mymall01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mymall01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

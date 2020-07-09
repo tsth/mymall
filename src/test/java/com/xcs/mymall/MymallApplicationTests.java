6fd3d47018c1216dc63e8fc211a7bf64495009a8
tsth <1349810446@qq.com>
2020-07-09 22:03:44 +0800

    创建项目

---
+++ src/test/java/com/xcs/mymall/MymallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcs.mymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
